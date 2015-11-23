59a8dbba40063cc8a898580d4dc377e512ec5b22
toanhtran <toanh_tran@yahoo.com>
2015-11-23 21:28:05 +0900

    add blog app

--- netmag/settings.py
+++ netmag/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'blog',
 )
 
 MIDDLEWARE_CLASSES = (
